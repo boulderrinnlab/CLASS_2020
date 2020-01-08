1a7510e150210ffdee71f7f9b202bf2259a3cfc2
boulderrinnlab <54148674+boulderrinnlab@users.noreply.github.com>
2020-01-08 12:00:27 -0700

    Add files via upload

---
+++ run.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+#SBATCH -p long
+#SBATCH --job-name=k562_chip
+#SBATCH --mail-type=END,FAIL
+#SBATCH --mail-user=john.rinn@colorado.edu
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --mem=6gb
+#SBATCH --time=100:00:00
+#SBATCH --output=nextflow.out
+#SBATCH --error=nextflow.err
+
+pwd; hostname; date
+echo "Nice -- you've requested $SLURM_CPUS_ON_NODE core."
+
+module load singularity/3.1.1
+
+nextflow run nf-core/chipseq -r 1.1.0 \
+-profile singularity \
+--input DESIGN_BIGlebowski_fixed.csv \
+--fasta /Shares/rinn_class/data/genomes/human/gencode/v32/GRCh38.p13.genome.fa \
+--gtf /Shares/rinn_class/data/genomes/human/gencode/v32/gencode.v32.annotation.gtf \
+--macs_gsize 2.7e9 \
+--blacklist hg38-blacklist.v2.bed \
+--save_reference \
+--email john.rinn@colorado.edu \
+-resume \
+-c nextflow.config
+
+date
