36d8aa1d2418867c241c6df349b4372d3fb6ebe5
Michael Smallegan <michael.smallegan@colorado.edu>
2020-06-15 13:52:29 -0600

    Adding knitted markdown for 02 altough there are alot of extra plots that need to be cleaned up.

--- analysis/analysis.R
+++ analysis/analysis.R
@@ -8,3 +8,8 @@ library(rmarkdown)
 
 # 01_consensus_peaks
 rmarkdown::render("01_consensus_peaks/01_consensus_peaks.Rmd", md_document(variant = "markdown_github"))
+
+
+# 02_permutation_of_consensus_peaks
+rmarkdown::render("02_permutation_of_consensus_peaks/permutation_feature_intersects.Rmd", md_document(variant = "markdown_github"))
+  
