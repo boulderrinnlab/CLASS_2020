2fa9bba71f7661dbbe266a53c59269278cc8199f
Michael Smallegan <michael.smallegan@colorado.edu>
2020-06-15 16:33:45 -0600

    Adding a potential new script for annotating the DBPs using harmonizome. The Human TFs data would also go in here.

--- analysis/analysis.R
+++ analysis/analysis.R
@@ -13,3 +13,6 @@ rmarkdown::render("01_consensus_peaks/01_consensus_peaks.Rmd", md_document(varia
 # 02_permutation_of_consensus_peaks
 rmarkdown::render("02_permutation_of_consensus_peaks/permutation_feature_intersects.Rmd", md_document(variant = "markdown_github"))
   
+
+# 03_global_clustering
+rmarkdown::render("03_global_clustering/03_global_clustering.Rmd", md_document(variant = "markdown_github"))
